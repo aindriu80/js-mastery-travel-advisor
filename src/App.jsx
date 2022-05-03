fee053721364faf65304875624ac2d8b6e18fd34
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-03 08:12:52 +0100

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Travel Advisor</h2>
+    </div>
+  )
+}
+
+export default App
